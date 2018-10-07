35556510bcb2b1c6a1403062cffca3a7f47cc4e6
Mac <mac@MacBook-Pro.fios-router.home>
2018-10-06 22:23:07 -0500

    implemented customized admin dashboard user dashboards

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -10,10 +10,12 @@ class AdminUserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     email: Field::String,
-    encrypted_password: Field::String,
-    reset_password_token: Field::String,
-    reset_password_sent_at: Field::DateTime,
-    remember_created_at: Field::DateTime,
+    password: Field::String,
+    sign_in_count: Field::Number,
+    current_sign_in_at: Field::DateTime,
+    last_sign_in_at: Field::DateTime,
+    current_sign_in_ip: Field::String.with_options(searchable: false),
+    last_sign_in_ip: Field::String.with_options(searchable: false),
     first_name: Field::String,
     last_name: Field::String,
     age: Field::String,
@@ -23,6 +25,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
     cohort: Field::Number,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
+    type: Field::String,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -31,10 +34,9 @@ class AdminUserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
+    # :courses,
+    # :cohorts,
     :email,
-    :encrypted_password,
-    :reset_password_token,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -42,10 +44,11 @@ class AdminUserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :current_sign_in_ip,
+    :last_sign_in_ip,
     :first_name,
     :last_name,
     :age,
@@ -55,6 +58,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
     :cohort,
     :created_at,
     :updated_at,
+    :type,
   ].freeze
 
   # FORM_ATTRIBUTES
@@ -62,10 +66,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
+    :password,
     :first_name,
     :last_name,
     :age,
@@ -75,10 +76,4 @@ class AdminUserDashboard < Administrate::BaseDashboard
     :cohort,
   ].freeze
 
-  # Overwrite this method to customize how admin users are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(admin_user)
-  #   "AdminUser ##{admin_user.id}"
-  # end
 end
--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -10,10 +10,12 @@ class UserDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     email: Field::String,
-    encrypted_password: Field::String,
-    reset_password_token: Field::String,
-    reset_password_sent_at: Field::DateTime,
-    remember_created_at: Field::DateTime,
+    password: Field::String,
+    sign_in_count: Field::Number,
+    current_sign_in_at: Field::DateTime,
+    last_sign_in_at: Field::DateTime,
+    current_sign_in_ip: Field::String.with_options(searchable: false),
+    last_sign_in_ip: Field::String.with_options(searchable: false),
     first_name: Field::String,
     last_name: Field::String,
     age: Field::String,
@@ -23,6 +25,7 @@ class UserDashboard < Administrate::BaseDashboard
     cohort: Field::Number,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
+    type: Field::String,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -31,10 +34,11 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    # :courses,
+    # :cohorts,
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
+    :type,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -42,10 +46,11 @@ class UserDashboard < Administrate::BaseDashboard
   SHOW_PAGE_ATTRIBUTES = [
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :current_sign_in_ip,
+    :last_sign_in_ip,
     :first_name,
     :last_name,
     :age,
@@ -55,6 +60,7 @@ class UserDashboard < Administrate::BaseDashboard
     :cohort,
     :created_at,
     :updated_at,
+    :type,
   ].freeze
 
   # FORM_ATTRIBUTES
@@ -62,10 +68,7 @@ class UserDashboard < Administrate::BaseDashboard
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
+    :password,
     :first_name,
     :last_name,
     :age,
