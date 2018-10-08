7cff937a592f8e73ae1b2c186674b3f5e4868c49
Mac <mac@MacBook-Pro.fios-router.home>
2018-10-08 18:30:52 -0500

    adjusted seeds file, courses and cohorts are not loading

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-AdminUser.create(email: "admin@test.com", password: "123123", password_confirmation: "123123", first_name: "Marge", last_name: "Simpson", age: "44", education: "HS")
+AdminUser.create(email: "admin@test.com", password: "123123", password_confirmation: "123123", first_name: "Marge", last_name: "Simpson", age: "44", education: "HS", url: "marge.jpg")
 
-User.create(email: "elizabeth@test.com", password: "123123", password_confirmation: "123123", first_name: "Elizabeth", last_name: "Hoover", age: "33", education: "BS Chemistry", url: "assets/images/Elizabeth_hoover.png", course: "1", type: "instructor")
-User.create(email: "burns@test.com", password: "123123", password_confirmation: "123123", first_name: "Mr", last_name: "Burns", age: "33", education: "MS", url: "assets/images/Mr_Burns.jpg", course: "2", type: "instructor")
-User.create(email: "ned@test.com", password: "123123", password_confirmation: "123123", first_name: "Ned", last_name: "Flanders", age: "31", education: "BS", url: "assets/images/Ned_Flanders.png", course: "2", type: "instructor")
-User.create(email: "apu@test.com", password: "123123", password_confirmation: "123123", first_name: "Apu", last_name: "LongName", age: "32", education: "Culinary Art", url: "assets/images/Apu.png", course: "3", type: "instructor")
-User.create(email: "homer@test.com", password: "123123", password_confirmation: "123123", first_name: "Homer", last_name: "Simpson", age: "40", education: "Not clear", url: "assets/images/homer.jpg", course: "3", type: "instructor")
-User.create(email: "krusty@test.com", password: "123123", password_confirmation: "123123", first_name: "Krusty", last_name: "Clown", age: "45", education: "None", url: "assets/images/Krusty.png", course: "1", type: "instructor")
+User.create(email: "elizabeth@test.com", password: "123123", password_confirmation: "123123", first_name: "Elizabeth", last_name: "Hoover", age: "33", education: "BS Chemistry", url: "assets/images/Elizabeth_hoover.png", course: "1")
+User.create(email: "burns@test.com", password: "123123", password_confirmation: "123123", first_name: "Mr", last_name: "Burns", age: "33", education: "MS", url: "assets/images/Mr_Burns.jpg", course: "2")
+User.create(email: "ned@test.com", password: "123123", password_confirmation: "123123", first_name: "Ned", last_name: "Flanders", age: "31", education: "BS", url: "assets/images/Ned_Flanders.png", course: "2")
+User.create(email: "apu@test.com", password: "123123", password_confirmation: "123123", first_name: "Apu", last_name: "LongName", age: "32", education: "Culinary Art", url: "assets/images/Apu.png", course: "3")
+User.create(email: "homer@test.com", password: "123123", password_confirmation: "123123", first_name: "Homer", last_name: "Simpson", age: "40", education: "Not clear", url: "assets/images/homer.jpg", course: "3")
+User.create(email: "krusty@test.com", password: "123123", password_confirmation: "123123", first_name: "Krusty", last_name: "Clown", age: "45", education: "None", url: "assets/images/Krusty.png", course: "1",)
 
 User.create(email: "lisa@test.com", password: "123123", password_confirmation: "123123", first_name: "Lisa", last_name: "Simpson", age: "19", education: "HS", url: "assets/images/Lisa.jpg", cohort: "1")
 User.create(email: "milhouse@test.com", password: "123123", password_confirmation: "123123", first_name: "Milhouse", last_name: "Van Houten", age: "20", education: "HS", url: "assets/images/milhouse.png", cohort: "2")
@@ -21,9 +21,13 @@ Course.create(name: "SEI", hours: "95")
 Course.create(name: "UIUX", hours: "85")
 Course.create(name: "WebDesign", hours: "90")
 
+puts "3 courses created"
+
 Cohort.create(name: "Methods1", start_date: "2018-05-11", end_date: "2018-08-22", course_id: "1", instructor: "Mr. Burns")
 Cohort.create(name: "Methods2", start_date: "2018-06-11", end_date: "2018-08-12", course_id: "2", instructor: "Ned Flanders")
 Cohort.create(name: "Edgecase1", start_date: "2018-07-11", end_date: "2018-09-22", course_id: "1", instructor: "Ms. Hoover")
 Cohort.create(name: "Edgecase2", start_date: "2018-04-11", end_date: "2018-06-12", course_id: "3", instructor: "Apu")
 Cohort.create(name: "Function1", start_date: "2018-05-21", end_date: "2018-08-11", course_id: "3", instructor: "Homer Simpson")
 Cohort.create(name: "Function2", start_date: "2018-06-21", end_date: "2018-08-29", course_id: "1", instructor: "Krusty")
+
+puts "3 cohorts for each course created"
