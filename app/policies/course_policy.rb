4bd9c6240b93ff95b94175c53bd758544b262a80
Mac <mac@MacBook-Pro.fios-router.home>
2018-10-08 20:45:44 -0500

    implemented hide Edit option, doesn't work

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -20,4 +20,8 @@ class CoursePolicy < ApplicationPolicy
   def update?
     record.user_id == user.id || admin_types.include?(user.type)
   end
+
+  def admin?
+    admin_types.include?(user.type)
+  end
 end
