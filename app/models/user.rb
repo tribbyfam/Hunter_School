78bf03d7e0008b54c853f887e667ca38bb80cb27
Mac <mac@MacBook-Pro.fios-router.home>
2018-10-08 21:07:03 -0500

    set age for user to be less than 150

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # after_initialize :set_defaults
+  validates :age, numericality: { less_than: 150.0 }
+  
   def full_name
     first_name.capitalize + " " + last_name.capitalize
   end
