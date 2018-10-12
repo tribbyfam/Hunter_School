1c85b7fe3f1db8b2978cd4465c54f7aa77955ab6
Mac <mac@MacBook-Pro.fios-router.home>
2018-10-11 22:29:47 -0500

    styled student's pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -6,4 +6,19 @@ class StaticController < ApplicationController
     end
   end
  
+  def edit
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
